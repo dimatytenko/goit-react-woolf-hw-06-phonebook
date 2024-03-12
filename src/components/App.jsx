0fef0b1534af9600475c47cd11629f914ae91b9f
Dima Tytenko <amgpetronass@gmail.com>
2024-03-12 22:11:21 +0200

    fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,17 @@
-import { useDispatch, useSelector } from 'react-redux';
-
 import { ContactList } from './ContactList';
 import { Filter } from './Filter';
 import { ContactForm } from './ContactForm';
-import { addContact, deleteContact } from '../redux/contacts/contactsSlice';
-import { changeFilter } from '../redux/contacts/filterSlice';
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(state => state.contacts.contacts);
-  const filter = useSelector(state => state.filter);
-
-  const onDeleteContact = id => {
-    dispatch(deleteContact(id));
-  };
-
-  const onChangeFilter = e => {
-    dispatch(changeFilter(e.target.value));
-  };
-
-  const filterContacts = (filter, contacts) => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  };
-
-  const addNewContact = newContact => {
-    if (
-      contacts.some(
-        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-      )
-    ) {
-      alert(`${newContact.name} is already in contacts`);
-      return false;
-    }
-    dispatch(addContact(newContact));
-    return true;
-  };
-
   return (
     <div className="App">
       <h1>Phonebook</h1>
 
-      <ContactForm onSubmit={addNewContact} />
+      <ContactForm />
 
       <h2>Contacts</h2>
-      <Filter
-        value={useSelector(state => state.contacts.filter)}
-        onChange={onChangeFilter}
-      />
-      <ContactList
-        list={filterContacts(filter, contacts)}
-        onDeleteContact={onDeleteContact}
-      />
+      <Filter />
+      <ContactList />
     </div>
   );
 };
